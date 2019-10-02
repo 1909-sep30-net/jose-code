643c1ad4d04b337be43f7a78d9bbfca26e684e14
jfarro01 <jfarro01.jose.nicolas.farro.a@gmail.com>
2019-10-02 12:15:42 -0500

    Uploading ShapesApp

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FizzCount = 0;
+            int BuzzCount = 0;
+            int FizzbuzzCount = 0;
+
+            for(int i = 0; i <= 1000; i++)
+            {
+                if(i%3 == 0 && i%5 != 0){
+                    Console.WriteLine("Fizz");
+                    FizzCount++;
+                }
+                else if(i%5 == 0 && i%3 != 0){
+                    System.Console.WriteLine("Buzz");
+                    BuzzCount++;
+                }
+                else if(i%5 == 0 && i%3 ==0){
+                    System.Console.WriteLine("Fizzbuzz");
+                    FizzbuzzCount++;
+                }
+                else
+                    System.Console.WriteLine(i);
+            }
+            System.Console.WriteLine("Fizz Count: " + FizzCount + " Buzz Count: " + BuzzCount + " Fizzbuzz Count: " + FizzbuzzCount);
+        }
+    }
+}
