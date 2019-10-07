cb83783b93fa7b67543ac987c4762409ad8b315a
jfarro01 <jfarro01.jose.nicolas.farro.a@gmail.com>
2019-10-07 11:20:45 -0500

    Added Palindrome class, functionality, and added Xunit testing

---
+++ Assessment-Week1/Assessment-Week1.Library/Palindrome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment_Week1.Library
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string word)
+        {
+            char[] reverse = word.ToCharArray();
+            Array.Reverse(reverse);
+            if (word.ToCharArray().Equals(reverse))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Assessment-Week1/Assessment-Week1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Assessment_Week1.Library;
+using Xunit;
+
+namespace Assessment_Week1
+{
+    public class Program
+    {
+        [Fact]
+        public void PalindromeShouldPalindrome()
+        {
+            var test = new Palindrome();
+
+            test.IsPalindrome("Anna");
+
+            Assert.Equal(expected:true)
+        }
+    }
+}
