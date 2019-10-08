5b52a5536b42879f59a3c042eca8ec9e2d5a9aa4
jfarro01 <jfarro01.jose.nicolas.farro.a@gmail.com>
2019-10-08 13:24:31 -0500

    Added basic sql exercises

---
+++ SQL/2-exercises-basic.sql
@@ -0,0 +1,46 @@
+-- basic exercises (Chinook database)
+
+-- 1. list all customers (full names, customer ID, and country) who are not in the US
+select 
+	FirstName + ' ' + LastName as [Full Name],
+	CustomerId as [Customer ID],
+	Country as [Country]
+from Customer
+where Country != 'USA';
+
+
+-- 2. list all customers from brazil
+select 
+	FirstName + ' ' + LastName as [Customer]
+from Customer
+where Country = 'Brazil';
+
+-- 3. list all sales agents
+select 
+	FirstName + ' ' + LastName as [Sales Agents]
+from Employee
+where Title = 'Sales Support Agent'
+
+-- 4. show a list of all countries in billing addresses on invoices.
+select 
+	BillingCountry as [Billing Countries on invoices]
+from Invoice;
+
+-- 5. how many invoices were there in 2009, and what was the sales total for that year?
+--    (extra challenge: find the invoice count sales total for every year, using one query)
+select 
+	--InvoiceDate as [Invoice Date in 2009],
+	InvoiceDate as [Invoice Date in 2009],
+	sum (Total)  as [Sales total in 2009]
+from Invoice
+where InvoiceDate like '%2009%'
+group by InvoiceDate;
+
+-- 6. how many line items were there for invoice #37?
+select 
+from Invoice
+
+
+-- 7. how many invoices per country?
+
+-- 8. show total sales per country, ordered by highest sales first.
